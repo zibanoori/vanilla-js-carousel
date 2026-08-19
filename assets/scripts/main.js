788d8872826b3d9322ba9239a2e978e9f5f4865b
zibanoori <noorizibaa@gmail.com>
2026-08-19 20:03:17 +0330

    Connect JavaScript and add slide navigation

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,36 +1,42 @@
-const slider = document.querySelectorAll(".slide");
+const slides = document.querySelectorAll(".slide");
 const dots = document.querySelectorAll(".dot");
-const prevBtn = document.querySelectorAll(".prev");
-const nextBtn = document.querySelectorAll(".next");
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
 
 let currentSlide = 0;
 
 function showSlide(index) {
-    slides,forEach((slide) => {
-        slide.clasList.remove("active");
+    slides.forEach((slide) => {
+        slide.classList.remove("active");
     });
+
     dots.forEach((dot) => {
         dot.classList.remove("active");
     });
-    slide[index].clasList.add("active");
-    dots[index].classList.add("avtive");
+
+    slides[index].classList.add("active");
+    dots[index].classList.add("active");
 }
 
-nextBtn.addEventListener("click",() =>{
+
+nextBtn.addEventListener("click", () => {
     currentSlide++;
 
-    if (currentSlide >= slide.length) {
+    if (currentSlide >= slides.length) {
         currentSlide = 0;
     }
 
     showSlide(currentSlide);
 });
 
-prevBtn.addEventListener("click",() =>{
+prevBtn.addEventListener("click", () => {
     currentSlide--;
+
     if (currentSlide < 0) {
         currentSlide = slides.length - 1;
     }
 
     showSlide(currentSlide);
 });
+
+console.log("JS connected");
