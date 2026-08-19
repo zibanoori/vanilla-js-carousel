6cebd5ac7f15854758f1cc35a7b6adfdad089577
zibanoori <noorizibaa@gmail.com>
2026-08-19 18:28:44 +0330

    Getting the next buttons in JavaScript

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,4 @@
 const slider = document.querySelectorAll(".slide");
 const dots = document.querySelectorAll(".dot");
 const prevBtn = document.querySelectorAll(".prev");
+const nextBtn = document.querySelectorAll(".next")
