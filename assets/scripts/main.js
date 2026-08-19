cf4154f5f4ba3dea19951afe4703c46e942c2aac
zibanoori <noorizibaa@gmail.com>
2026-08-19 18:47:58 +0330

    Writing a function and deleting the previous active class

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,6 +1,12 @@
 const slider = document.querySelectorAll(".slide");
 const dots = document.querySelectorAll(".dot");
 const prevBtn = document.querySelectorAll(".prev");
-const nextBtn = document.querySelectorAll(".next")
+const nextBtn = document.querySelectorAll(".next");
 
 let currentSlide = 0;
+
+function showSlide(index) {
+    slides,forEach((slide) => {
+        slide.clasList.remove("active");
+    })
+}
