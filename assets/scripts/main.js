666fae020050bb78e726b4380920506af8c9be51
zibanoori <noorizibaa@gmail.com>
2026-08-19 18:30:09 +0330

    Creating a variable for the first slide

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,3 +2,5 @@ const slider = document.querySelectorAll(".slide");
 const dots = document.querySelectorAll(".dot");
 const prevBtn = document.querySelectorAll(".prev");
 const nextBtn = document.querySelectorAll(".next")
+
+let currentSlide = 0;
